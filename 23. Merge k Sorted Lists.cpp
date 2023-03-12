c53907a78275a35fe683e3b2a0d543c138354499
FalendraDewangan <59726848+FalendraDewangan@users.noreply.github.com>
2023-03-12 16:48:16 +0530

    Merge sort approach added

--- 23. Merge k Sorted Lists.cpp
+++ 23. Merge k Sorted Lists.cpp
@@ -1,6 +1,8 @@
 // Leetcode problem link
 // https://leetcode.com/problems/merge-k-sorted-lists/
 
+
+// Solution 1
 // Time complexity : O(nlogk)
 // pace complexity : O(k)
 
@@ -44,5 +46,121 @@ public:
                 pq.push(tail->next);
         }
         return head;
+    }
+};
+
+// Solution 2
+class Solution {
+    ListNode* mergeTwoSortedList(ListNode* l1, ListNode* l2)
+    {
+        if(l1 == NULL)
+            return l2;
+        if(l2 == NULL)
+            return l1;
+        
+        if(l1->val <= l2->val)
+        {
+            l1->next = mergeTwoSortedList(l1->next, l2);
+            return l1;
+        }
+        else{
+            l2->next = mergeTwoSortedList(l1, l2->next);
+            return l2;
+        }
+    }
+
+    ListNode* mergeList(vector<ListNode*>& lists, int s, int e)
+    {
+        if(s==e)
+            return lists[s];
+
+        int mid = s + (e-s)/2;
+        ListNode* l1 =  mergeList(lists, s, mid);
+        ListNode* l2 = mergeList(lists, mid+1, e);
+
+        return mergeTwoSortedList(l1, l2);
+    }
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size();
+        if(k==0)
+            return NULL;
+
+        return mergeList(lists, 0, k-1);
+    }
+};
+
+// Solution 3
+class Solution {
+    ListNode* mergeTwoSortedList(ListNode* l1, ListNode* l2)
+    {
+        if(l1 == NULL)
+            return l2;
+
+        if(l2 == NULL)
+            return l1;
+
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+
+        while(l1 && l2)
+        {
+            if(l1->val <= l2->val)
+            {
+                if(tail)
+                {
+                    tail->next = l1;
+                    tail = tail->next;
+                    l1 = l1->next;
+                }
+                else{
+                    head = tail = l1;
+                    l1 = l1->next;
+                }
+                
+            }
+            else
+            {
+                if(tail)
+                {
+                    tail->next = l2;
+                    tail = tail->next;
+                    l2 = l2->next;
+                }
+                else{
+                    head = tail = l2;
+                    l2 = l2->next;
+                }
+                
+            }
+        }
+
+        if(l1)
+            tail->next = l1;
+
+        else if(l2)
+            tail->next = l2;
+        
+        return head;
+    }
+
+    ListNode* mergeList(vector<ListNode*>& lists, int s, int e)
+    {
+        if(s==e)
+            return lists[s];
+
+        int mid = s + (e-s)/2;
+        ListNode* l1 =  mergeList(lists, s, mid);
+        ListNode* l2 = mergeList(lists, mid+1, e);
+
+        return mergeTwoSortedList(l1, l2);
+    }
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size();
+        if(k==0)
+            return NULL;
+
+        return mergeList(lists, 0, k-1);
     }
 };
