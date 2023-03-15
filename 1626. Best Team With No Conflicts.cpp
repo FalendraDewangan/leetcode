abe91962b84efb26b61e93a5220e8339c0b9f73a
FalendraDewangan <59726848+FalendraDewangan@users.noreply.github.com>
2023-03-16 00:25:51 +0530

    ADDING OPTIMAL APPROACH - Medium Level

--- 1626. Best Team With No Conflicts.cpp
+++ 1626. Best Team With No Conflicts.cpp
@@ -1,36 +1,22 @@
 // Leetcode problem link
 // https://leetcode.com/problems/best-team-with-no-conflicts/
 
+// Solution 1 : Dynamic programmig
 class Solution {
-    int solveRec(vector<pair<int, int>>& players, int i, int maxIndex)
+    int solveDP(vector<pair<int, int>>& players, int i, int maxi, vector<vector<int>>& dp)
     {
-        if(i < 0)
+        if(i >= players.size())
             return 0;
 
-        int ans = 0;
-        if(players[i].second <= players[maxIndex].second)
-            ans = players[i].second + solveRec(players, i-1, i);
-
-        ans = max(ans, solveRec(players, i-1, maxIndex));
-
-        return ans;
-    }
-
-    int solveDP(vector<pair<int, int>>& players, int i, int maxIndex, vector<vector<int>>& dp)
-    {
-        if(i < 0)
-            return 0;
-
-        if(dp[i][maxIndex] != -1)
-            return dp[i][maxIndex];
+        if(dp[i][maxi] != -1)
+            return dp[i][maxi];
 
         int ans = 0;
-        if(players[i].second <= players[maxIndex].second)
-            ans = players[i].second + solveDP(players, i-1, i, dp);
-
-        ans = max(ans, solveDP(players, i-1, maxIndex, dp));
-
-        return dp[i][maxIndex] = ans;
+        if(players[maxi].second <= players[i].second)
+            ans = players[i].second + solveDP(players, i+1, i, dp);
+        
+        ans = max(ans, solveDP(players, i+1, maxi, dp));
+        return dp[i][maxi] = ans;
     }
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
@@ -39,15 +25,40 @@ public:
         for(int i=0; i<n; i++)
             players[i] = {ages[i], scores[i]};
         
+        players.push_back({0, 0});
         sort(players.begin(), players.end());
-        players.push_back({INT_MAX, INT_MAX});
 
-        // // Solution 1 : using recursion
-        // return solveRec(players, n-1, n);
+        // Dynamic programming
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return solveDP(players, 1, 0, dp);
+    }
+};
+
+// Solution 2 : Optimal solution
+class Solution {
+public:
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        int n = scores.size();
+        vector<pair<int, int>> players(n);
+        for(int i=0; i<n; i++)
+            players[i] = {ages[i], scores[i]};
+        
+        sort(players.begin(), players.end());
 
 
-        // Solution 2: Using dynamic programming
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return solveDP(players, n-1, n, dp);
+        vector<int> dp(n, 0);
+        int ans = 0;
+        for(int i=0; i<n; i++)
+        {
+            int score = players[i].second;
+            for(int j=i-1; j>=0; j--)
+            {
+                if(players[j].second <= players[i].second)
+                    score = max(score, players[i].second + dp[j]);
+            }
+            dp[i] = score;
+            ans = max(ans, score);
+        }
+        return ans;
     }
 };
