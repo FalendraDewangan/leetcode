dea55f5b4b723d13c989b4a5e9c3399ca313c06f
FalendraDewangan <59726848+FalendraDewangan@users.noreply.github.com>
2023-03-24 10:11:58 +0530

    Medium Level

---
+++ 2348. Number of Zero-Filled Subarrays.cpp
@@ -0,0 +1,29 @@
+// Leetcode problem link
+// https://leetcode.com/problems/number-of-zero-filled-subarrays/
+
+// O(N) Time complexity
+// O(N) Space complexity
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long res = 0;
+        int siz = nums.size();
+        for(int i=0; i<siz; )
+        {
+            if(nums[i]==0)
+            {
+                int j = i;
+                while(j<siz && nums[j]==0)
+                    j++;
+                
+                long long n = j - i;
+                res += (n*(n+1))/2;
+                i = j;
+            }
+            else 
+                i++;
+        }
+        return res;
+    }
+};
